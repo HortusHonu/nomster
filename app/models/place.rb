5fb298d86be33dff8128bafc7a98dfcb45fb2730
Eric <james.eric.knight@gmail.com>
2018-01-21 00:26:39 +0000

    Comments in database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
 	belongs_to :user
+	has_many :comments
+	
 	validates :name, presence: true, length: { minimum: 3 }
 	validates :address, :description, presence: true
 	geocoded_by :address
