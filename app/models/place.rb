7691d12b05de00e0ddfe53356d5149e8329789b9
Eric <james.eric.knight@gmail.com>
2018-02-09 21:58:44 +0000

    dependent fix

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
-	has_many :photos, dependent: :destroy
+	has_many :photos
 	
 	validates :name, presence: true, length: { minimum: 3 }
 	validates :address, :description, presence: true
